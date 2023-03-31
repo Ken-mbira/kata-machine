608ab7df29fb3004cdd0fde7cd3f116d0aa95265
Ken Mbira <devmbira@gmail.com>
2023-03-31 11:54:49 +0300

    Implement doubly linked list

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -38,17 +38,85 @@ export default class DoublyLinkedList<T> {
 
 }
     append(item: T): void {
+        let newNode : Node<T> = { value : item };
 
-}
+        if(this.length == 0){
+            // if empty
+            this.head = newNode;
+        }else if(this.length == 1){
+            // if only one item
+            newNode.previous = this.head;
+            this.tail = newNode;
+            this.head!.next = this.tail;
+        }else{
+            newNode.previous = this.tail;
+            this.tail!.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+    }
     remove(item: T): T | undefined {
-return undefined;
-}
+        let currentNode = this.head;
+        for(let i=0; i<this.length; i++){
+            if(currentNode?.value == item){
+                return this.removeAt(i);
+            }
+        }
+        return undefined;
+    }
     get(idx: number): T | undefined {
+        let currentNode = this.head;
 
-return undefined;
-}
+        for(let i=0; i<idx; i++){
+           currentNode = currentNode?.next; 
+        }
+
+        return currentNode?.value;
+    }
+
     removeAt(idx: number): T | undefined {
+        if(idx >= this.length){
+            return undefined;
+        }
 
-return undefined;
-}
+        if(idx == 0){
+            // if its head
+            let foundValue = this.head?.value;
+
+            if(this.length == 2){
+                this.tail = undefined;
+            }
+            this.head = this.head?.next;
+
+            this.length--;
+
+            return foundValue;
+        }
+
+        if(idx == (this.length-1)){
+            // if its tail
+            let foundValue = this.tail?.value;
+            
+            if(this.length > 2){
+                this.tail = this.tail?.previous;
+            }
+            this.length --;
+            return foundValue; 
+        }
+
+        let currentNode = this.head;
+
+        for(let i=0; i<idx; i++){
+           currentNode = currentNode?.next; 
+        }
+
+        let previousNode = currentNode?.previous;
+        let nextNode = currentNode?.next;
+
+        currentNode!.previous!.next = nextNode;
+        currentNode!.next!.previous = previousNode;
+        this.length--;
+        return currentNode?.value;
+    }
 }
